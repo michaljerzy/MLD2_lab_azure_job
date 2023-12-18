c79d45c8847d0fed54178d177ff9778f0f3a567b
Michal Andrzejewski <michal.jerzy99@gmail.com>
2023-12-18 21:10:54 +0100

    Changes

--- src/model/train.py
+++ src/model/train.py
@@ -13,8 +13,10 @@ from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import train_test_split
 
+
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin',
+               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
